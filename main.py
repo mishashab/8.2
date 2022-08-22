1fb92c6bae7f528c81c6c4f68af03c96bc5045af
mishashab <110331360+mishashab@users.noreply.github.com>
2022-08-22 16:23:55 +0300

    Update yandexDisk.py

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-import requests
 from yandexDisk import YaUploader
 import os
+
+token = ""
+
+
+def get_files_from_directory():
+    # Если вводить вручную
+    # path_to_directory = input('Введите путь директории')
+    # Для быстрой отладки
+    path_to_file = os.path.join(os.getcwd(), 'output_files')
+    paths_to_files = []
+    for file_name in os.listdir(path_to_file):
+        paths_to_files.append(os.path.join(path_to_file, file_name))
+    return paths_to_files
+
+
+def upload_files_to_yandex_disk(paths):
+    uploader = YaUploader(token)
+    for file_paths in paths:
+        uploader.upload(file_paths)
+
+
 if __name__ == '__main__':
     # Получить путь к загружаемому файлу и токен от пользователя
-    # path_to_file = input()
-    # Для быстрой отладки
-    path_to_file = os.path.join(os.getcwd(), '1.txt')
-    token = "y0_AgAAAAAIX_RZAADLWwAAAADMy68nbBBNWYDHRl6DGrNSZa6PJvVBTb0"
-    uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
+    files_paths: list = get_files_from_directory()
+    upload_files_to_yandex_disk(files_paths)
--- yandexDisk.py
+++ yandexDisk.py
@@ -1,4 +1,6 @@
 import requests
+from pprint import pprint
+
 
 class YaUploader:
     def __init__(self, token: str):
@@ -7,13 +9,15 @@ class YaUploader:
     def get_headers(self):
         return {
             'Content-Type': 'application/json',
-            'Autorization': f'OAuth {self.token}'
+            'Authorization': f'OAuth {self.token}',
         }
+
 
     def upload(self, file_path: str):
         """Метод загружает файлы по списку file_list на яндекс диск"""
-#        files_url = "https://cloud-api.yandex.net:443/v1/disk/resources/files"
-        files_url = "https://cloud-api.yandex.net/v1/disk/resources/files"
+        host = "https://cloud-api.yandex.net:443/v1/disk/resources/upload"
+        params = {"path": file_path.split('\\')[-1], "owerwrite": "true"}
         headers = self.get_headers()
-        response = requests.get(files_url, headers)
-        print(response.json())
+        response = requests.get(host, headers=headers, params=params)
+        href = response.json().get('href')
+        response = requests.put(href, data=open(file_path, 'rb'))
